29722c981f9c1128e4890819c1197cc945f523ea
Tomasz Zwierzchowski <tzwierzchowski@atlassian.com>
2018-10-11 14:03:14 +0200

    Stepper

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
+#include "stepper.h"
 
 void setup() {
-    // put your setup code here, to run once:
+    Stepper motor1 = {D1, D2, D3, D4};
+    Stepper motor2 = {D5, D6, D7, D8, 0};
+
+    stepperOff(motor1);
+    stepperOff(motor2);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
 }
